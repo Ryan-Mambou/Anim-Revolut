5e49ff40ccbb6369bd85c349a6a7a9e5d7be10eb
Ryan Djemtchemeu <ryanmambou@gmail.com>
2024-01-02 07:43:31 +0100

    animation for slider

--- main.js
+++ main.js
@@ -3,36 +3,40 @@ import gsap from "gsap";
 let animate = document.querySelector(".animate")
 let scrollToTop = document.querySelector(".scroll-to-top")
 
+let reveal = document.querySelector('reveal')
 
 animate.addEventListener('click', (e) => {
-  console.log('click')
   gsap.to('.box', {
-    width: '100%',
+    width: '90vw',
     height: '100vh', 
     scale: 1, 
     borderRadius: '0px', zIndex: 5 })
   gsap.to('.middle-card', {
-    transform: "scale(0.7)",
+    transform: "scale(0.7) translateX(-72%)",
     borderRadius: '2.5rem',
-    border: "none"
+    border: "none",
   })
-  gsap.to('reveal', {
+  gsap.to('.reveal', {
+    opacity: 1,
+    zIndex: 10
+  })
+  gsap.to('.reveal-text', {
+    opacity: 1,
+    zIndex: 10
+  })
+  gsap.to('.reveal-slider', {
+    transform: "translateY(-50px)",
+    zIndex: 10
+  })
+  gsap.to('.secondary-one', {
+    borderRadius: '1110px',
+    transform: "translateX(96%) scale(0.6)",
     opacity: 1
   })
-  gsap.to('reveal-text', {
+  gsap.to('.secondary-two', {
+    borderRadius: '1110px',
+    transform: "translateX(-96%) scale(0.6)",
     opacity: 1
-  })
-  gsap.to('secondary-one', {
-    duration: 1,
-    borderRadius: '1110px',
-    backgroundColor: 'blue',
-    transform: "translateX('20%') scale(0.6)"
-  })
-  gsap.to('secondary-two', {
-    duration: 1,
-    borderRadius: '1110px',
-    backgroundColor: 'blue',
-    transform: "translateX('20%') scale(0.6)"
   })
 })
 
